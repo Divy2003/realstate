a72fff9ba2ee46d9074a29ec5ed6924cbe827fa7
Divy2003 <divypatel743@gmail.com>
2025-07-30 14:00:22 +0530

    last

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import '../styles/ongoingProjects.css';
+
 
 const ProjectCard = ({ project, index, inView, onClick }) => {
   const isOngoing = project.status === 'ongoing';
